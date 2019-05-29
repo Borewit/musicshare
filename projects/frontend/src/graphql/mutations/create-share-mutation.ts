5268baa63df3d842c334df54f2f33f7cb6bdd1a5
Yannick Stachelscheid <stachelscheid@addbots.de>
2019-05-29 11:23:24 +0200

    simplyfy useCreateShare hook

--- projects/frontend/src/graphql/mutations/create-share-mutation.ts
+++ projects/frontend/src/graphql/mutations/create-share-mutation.ts
@@ -1,22 +1,14 @@
-import {
-  IGetSharesData,
-  IGetSharesVariables,
-  GET_SHARES,
-  shareKeys
-} from "./../queries/shares-query";
-import { IShareData } from "./../types";
-import { MutationUpdaterFn } from "apollo-client";
-import { useCallback } from "react";
+import { shareKeys } from "./../queries/shares-query";
 import { IShare } from "../types";
 import { useMutation } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 
 export interface ICreateShareVariables {
-  name: string;
+	name: string;
 }
 
 export interface ICreateShareData {
-  createShare: IShare;
+	createShare: IShare;
 }
 
 export const CREATE_SHARE = gql`
@@ -27,28 +19,8 @@ export const CREATE_SHARE = gql`
 	}
 `;
 
-export const useCreateShare = ({ name }: any) => {
-  const updateShareCache = useCallback<MutationUpdaterFn<ICreateShareData>>(
-    (cache, { data }) => {
-      const currentShares = cache.readQuery<
-        IGetSharesData,
-        IGetSharesVariables
-      >({
-        query: GET_SHARES
-      })!.user.shares;
-
-      cache.writeQuery<IGetSharesData, IGetSharesVariables>({
-        query: GET_SHARES,
-        data: { user: { shares: currentShares.concat([data!.createShare]) } }
-      });
-    },
-    [name]
-  );
-
-  const hook = useMutation(CREATE_SHARE, {
-    variables: { name },
-    update: updateShareCache
-  });
-
-  return hook;
+export const useCreateShare = ({ name }: ICreateShareVariables) => {
+	return useMutation(CREATE_SHARE, {
+		variables: { name }
+	});
 };
